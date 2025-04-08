8ceed0c5fcd4e07377a70af18e64d7dc62923d2a
EstudiosVizcaino <estudiosvizcaino@gmail.com>
2025-04-08 19:56:53 +0200

    express save in middle of error fix

--- src/push_swap.c
+++ src/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: cvizcain <cvizcain@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/01 12:22:12 by cvizcain          #+#    #+#             */
-/*   Updated: 2025/04/08 04:24:25 by cvizcain         ###   ########.fr       */
+/*   Updated: 2025/04/08 19:53:48 by cvizcain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,27 @@ int	main(int argc, char **argv)
 	int	i;
 	int	j;
 	int	*nbs;
+	char	**temp;
 
-	i = 1;
+	i = 0;
 	j = 0;
 	if (argc < 2)
 		return (-1);
+	if (argc == 2 && (ft_split(argv[1], ' ')))
+	{
+		printf("PINGUITA\n");
+		nbs = sanitize_args((argc + 1), ft_split(argv[0], ' '));
+	}
+
+	// temp = ft_split(argv[1], ' ');
+
+	// printf("Strlen: %i\n", ft_strlen2(temp));
+	// while (j < ft_strlen2(temp))
+	// {
+	// 	nbs[j] = ft_atoi(temp[j]);
+	// 	printf("-> %i\n", nbs[j++]);
+	// }
+
 	nbs = sanitize_args(argc, argv);
 	if (!nbs)
 		return (-1);
--- src/utils/utils.c
+++ src/utils/utils.c
@@ -6,8 +6,19 @@
 /*   By: cvizcain <cvizcain@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/07 23:43:05 by cvizcain          #+#    #+#             */
-/*   Updated: 2025/04/08 04:16:26 by cvizcain         ###   ########.fr       */
+/*   Updated: 2025/04/08 19:48:28 by cvizcain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "utils.h"
+
+int	ft_strlen2(char **str)
+{
+	int	count;
+	int	j;
+
+	count = 0;
+	while (str[count])
+		++count;
+	return (count);
+}
