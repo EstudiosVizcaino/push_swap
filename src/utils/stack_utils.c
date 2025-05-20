ddf7c433392802e056b2b61a1306ef052aea2c9c
EstudiosVizcaino <estudiosvizcaino@gmail.com>
2025-05-20 17:10:11 +0200

    Created free_stack(t_stack_node **stack) in stack_utils.c

--- src/utils/stack_utils.c
+++ src/utils/stack_utils.c
@@ -6,7 +6,7 @@
 /*   By: cvizcain <cvizcain@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/24 13:43:29 by cvizcain          #+#    #+#             */
-/*   Updated: 2025/04/26 00:41:24 by cvizcain         ###   ########.fr       */
+/*   Updated: 2025/05/20 17:08:28 by cvizcain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,25 @@ void	stack_init(t_stack_node **stack, t_processed_input input)
 	node->next = NULL;
 	node->nbr = input.nbs[i];
 	*stack = node;
+	printf("\n");
+	while (i < input.length)
+	{
+		printf("%i | %i\t", input.nbs[i], get_position(input.index, input.nbs[i]));
+		++i;
+	}
+	printf("\n");
 	free (input.nbs);
 	free (node);
+}
+
+void	free_stack(t_stack_node **stack)
+{
+	t_stack_node	*tmp;
+
+	while (*stack)
+	{
+		tmp = (*stack)->next;
+		free(*stack);
+		*stack = tmp;
+	}
 }
