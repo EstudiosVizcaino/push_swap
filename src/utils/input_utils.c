1ba09fbfbdad26b394afa2d13b64087f256bcea3
EstudiosVizcaino <estudiosvizcaino@gmail.com>
2025-05-20 17:10:33 +0200

    Added get_position() to input_utils and to utils.h

--- src/utils/input_utils.c
+++ src/utils/input_utils.c
@@ -6,7 +6,7 @@
 /*   By: cvizcain <cvizcain@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/13 14:29:03 by cvizcain          #+#    #+#             */
-/*   Updated: 2025/05/13 14:52:51 by cvizcain         ###   ########.fr       */
+/*   Updated: 2025/05/20 17:08:16 by cvizcain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,27 @@ void	set_length(t_processed_input *input, int n)
 {
 	input->length = n;
 }
+
 int	*ft_get_index(t_processed_input input)
 {
 	int	*index;
 
 	index = input.index;
 	return (index);
+}
+
+int	get_position(int *nbs, int nb)
+{
+	int	position;
+	int	found;
+
+	position = 0;
+	found = 0;
+	while (!found)
+	{
+		if (nbs[position] == nb)
+			return (position);
+		++position;
+	}
+	return (-1);
 }
