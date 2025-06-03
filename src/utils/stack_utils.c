918c4dc72ecb417c5b13f7a1b2df4abe5b64dd71
EstudiosVizcaino <estudiosvizcaino@gmail.com>
2025-06-03 18:40:52 +0200

    Created `append_node()` in stack_utils

--- src/utils/stack_utils.c
+++ src/utils/stack_utils.c
@@ -6,36 +6,44 @@
 /*   By: cvizcain <cvizcain@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/24 13:43:29 by cvizcain          #+#    #+#             */
-/*   Updated: 2025/06/03 18:22:07 by cvizcain         ###   ########.fr       */
+/*   Updated: 2025/06/03 18:39:06 by cvizcain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "utils.h"
 
+static void	append_node(t_stack_node **stack, int n)
+{
+	t_stack_node	*node;
+	//t_stack_node	*last_node;
+
+	if (!stack)
+		return ;
+	node = ft_calloc(1, sizeof(t_stack_node));
+	if (!node)
+		return ;
+	node->next = NULL;
+	node->nbr = n;
+}
+
 void	stack_init(t_stack_node **stack_a, t_processed_input input)
 {
 	int				i;
-	t_stack_node	*node;
 	//t_stack_node	*last_node;
 	i = 0;
 	if (!input.nbs || !stack_a || input.length == -1)
 		return (free (input.nbs));
-	node = ft_calloc(1, sizeof(t_stack_node));
-	if (!node)
-		return (free (input.nbs));
-	node->next = NULL;
-	node->nbr = input.nbs[i];
-	*stack_a = node;
-	printf("\n");
+	printf("\n"); /** Shows received numbers with position in the array */
 	while (i < input.length)
 	{
 		printf("%i | %i\t", input.nbs[i],
 			get_position(input.index, input.nbs[i]));
 		++i;
 	}
+	if (0)
+		append_node(stack_a, 2);
 	printf("\n");
 	free (input.nbs);
-	free (node);
 }
 
 void	free_stack(t_stack_node *stack)
