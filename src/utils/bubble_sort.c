218d0c1b497292774fca11421607c0faa952fff8
EstudiosVizcaino <estudiosvizcaino@gmail.com>
2025-06-05 18:15:14 +0200

    Work in progress: bubble sort adaptation for stack implementation.

--- src/utils/bubble_sort.c
+++ src/utils/bubble_sort.c
@@ -6,7 +6,7 @@
 /*   By: cvizcain <cvizcain@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/12 20:42:45 by cvizcain          #+#    #+#             */
-/*   Updated: 2025/06/03 17:26:18 by cvizcain         ###   ########.fr       */
+/*   Updated: 2025/06/05 18:13:54 by cvizcain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,6 +52,29 @@ int	*bubble_sort(int *tab, int len)
 		i++;
 	}
 	return (temp);
+}
+
+void	*bubble_sort2(t_stack_node **stack)
+{
+	t_stack_node	*current;
+	t_stack_node	*tmp;
+
+	current = *stack;
+	while (!stack_sorted(*stack))
+	{
+		if (current->nbr > current->next->nbr)
+		{
+			tmp = current->next;
+			tmp->prev = current->prev;
+			tmp = tmp->next;
+			temp[j] = temp[j + 1];
+			temp[j + 1] = tmp;
+		last_node = find_last(*stack);
+		last_node->next = node;
+		node->prev = last_node;
+		cu
+		}
+	}
 }
 
 /**int	main(void)
