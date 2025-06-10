8caa0746152fa5c0a31b54adc9155761c1206535
EstudiosVizcaino <estudiosvizcaino@gmail.com>
2025-06-10 15:20:13 +0200

    Moved `free_stack()` to `utils.c`

--- src/utils/utils.c
+++ src/utils/utils.c
@@ -6,7 +6,7 @@
 /*   By: cvizcain <cvizcain@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/07 23:43:05 by cvizcain          #+#    #+#             */
-/*   Updated: 2025/05/13 14:58:51 by cvizcain         ###   ########.fr       */
+/*   Updated: 2025/06/10 15:15:20 by cvizcain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,4 +32,22 @@ void	ft_free_array(char **str)
 	while (str[i])
 		free(str[i++]);
 	free (str);
+}
+
+void	free_stack(t_stack_node **stack)
+{
+	t_stack_node	*tmp;
+	t_stack_node	*current;
+
+	if (!stack)
+		return ;
+	current = *stack;
+	while (current)
+	{
+		tmp = current->next;
+		current->nbr = 0;
+		free(current);
+		current = tmp;
+	}
+	*stack = NULL;
 }
