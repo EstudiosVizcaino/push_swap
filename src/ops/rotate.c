84df89d92534326521d95210c62e588b1dd850d6
EstudiosVizcaino <estudiosvizcaino@gmail.com>
2025-06-12 15:06:00 +0200

    First implementation of rotate

--- src/ops/rotate.c
+++ src/ops/rotate.c
@@ -6,16 +6,24 @@
 /*   By: cvizcain <cvizcain@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/10 14:10:06 by cvizcain          #+#    #+#             */
-/*   Updated: 2025/06/10 15:09:29 by cvizcain         ###   ########.fr       */
+/*   Updated: 2025/06/12 14:44:43 by cvizcain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "operations.h"
-
+/// @brief Rotates first node from top to bottom
+/// @param stack 
 static void	rotate(t_stack_node **stack)
 {
+	t_stack_node	*last_node;
 	if (!*stack || !(*stack)->next)
 		return ;
+	last_node = find_last(*stack);
+	last_node->next = *stack;
+	*stack = (*stack)->next;
+	(*stack)->prev = NULL;
+	last_node->next->prev = last_node;
+	last_node->next->next = NULL;
 }
 
 void	ra(t_stack_node **a)
