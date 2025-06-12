d8f5f31bf46013a2ee7159d6689f7359c39a18b9
EstudiosVizcaino <estudiosvizcaino@gmail.com>
2025-06-12 15:24:56 +0200

    Work in progress at `swap()`

--- src/ops/swap.c
+++ src/ops/swap.c
@@ -6,16 +6,19 @@
 /*   By: cvizcain <cvizcain@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/10 14:11:55 by cvizcain          #+#    #+#             */
-/*   Updated: 2025/06/10 15:10:03 by cvizcain         ###   ########.fr       */
+/*   Updated: 2025/06/12 15:18:20 by cvizcain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "operations.h"
-
-static void	swap(t_stack_node **stack)
+/// @brief Swaps places of first two nodes
+/// @param head 
+static void	swap(t_stack_node **head)
 {
-	if (!*stack || !(*stack)->next)
+	if (!*head || !(*head)->next)
 		return ;
+	(*head) = (*head)->next;
+	(*head)->prev->prev = 
 }
 
 void	sa(t_stack_node **a)
@@ -36,3 +39,11 @@ void	ss(t_stack_node **a, t_stack_node **b)
 	swap(b);
 	write (1, "ss\n", 3);
 }
+--------------------------------------------------
+
+		A 				B				C
+
+	*Head			Head->next		Head->next->next
+	prev: NULL		prev: A			prev: B
+	next: B			next: C			next: D 
+
