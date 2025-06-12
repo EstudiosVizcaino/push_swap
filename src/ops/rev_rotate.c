6c3cd2a7f69c5dd71ee275888a6ddbdc077668ca
EstudiosVizcaino <estudiosvizcaino@gmail.com>
2025-06-12 15:05:16 +0200

    First implementation of reverse rotate

--- src/ops/rev_rotate.c
+++ src/ops/rev_rotate.c
@@ -6,16 +6,25 @@
 /*   By: cvizcain <cvizcain@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/10 14:07:19 by cvizcain          #+#    #+#             */
-/*   Updated: 2025/06/10 15:09:19 by cvizcain         ###   ########.fr       */
+/*   Updated: 2025/06/12 15:04:47 by cvizcain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "operations.h"
-
+/// @brief Moves bottom node to head node
+/// @param stack 
 static void	reverse_rotate(t_stack_node **stack)
 {
+	t_stack_node	*last;
 	if (!*stack || !(*stack)->next)
 		return ;
+	last = find_last(*stack);
+	last->prev->next = NULL;
+	last->next = *stack;
+	last->prev = NULL;
+	*stack = last;
+	last->next->prev = last;
+
 }
 
 void	rra(t_stack_node **a)
