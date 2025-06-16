e80cb777fe30faa3274b7b129d886e0266d9e17b
EstudiosVizcaino <estudiosvizcaino@gmail.com>
2025-06-16 17:02:36 +0200

    Implemented `push()` in `push_swap.c`

--- src/ops/push.c
+++ src/ops/push.c
@@ -6,7 +6,7 @@
 /*   By: cvizcain <cvizcain@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/10 14:03:09 by cvizcain          #+#    #+#             */
-/*   Updated: 2025/06/10 14:16:46 by cvizcain         ###   ########.fr       */
+/*   Updated: 2025/06/16 16:57:43 by cvizcain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,26 @@
 
 static void	push(t_stack_node **dest, t_stack_node **src)
 {
-	if (!*dest || !*src)
+	t_stack_node	*push_node;
+
+	if (!*src)
 		return ;
+	push_node = *src;
+	*src = (*src)->next;
+	if (*src)
+		(*src)->prev = NULL;
+	push_node->prev = NULL;
+	if (!*dest)
+	{
+		*dest = push_node;
+		push_node->next = NULL;
+	}
+	else
+	{
+		push_node->next = *dest;
+		push_node->next->prev = push_node;
+		*dest = push_node;
+	}
 }
 
 void	pa(t_stack_node **a, t_stack_node **b)
