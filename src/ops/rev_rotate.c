ac45fd0884828f7711fd8c5570d8f65c6e33419d
EstudiosVizcaino <estudiosvizcaino@gmail.com>
2025-06-16 14:45:54 +0200

    Added doxygens to `rra()`, `rrb()` and `rrr()`

--- src/ops/rev_rotate.c
+++ src/ops/rev_rotate.c
@@ -6,7 +6,7 @@
 /*   By: cvizcain <cvizcain@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/10 14:07:19 by cvizcain          #+#    #+#             */
-/*   Updated: 2025/06/16 14:19:55 by cvizcain         ###   ########.fr       */
+/*   Updated: 2025/06/16 14:45:10 by cvizcain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,18 +41,45 @@ static void	reverse_rotate(t_stack_node **stack)
 	last->next->prev = last;
 }
 
+/**
+ * @brief Reverse rotates stack A downwards by one.
+ *
+ * The last element of stack A becomes the first one. This function
+ * performs the operation and writes "rra\n" to standard output.
+ *
+ * @param a Pointer to the pointer of the head of stack A.
+ */
 void	rra(t_stack_node **a)
 {
 	reverse_rotate(a);
 	write (1, "rra\n", 4);
 }
 
+/**
+ * @brief Reverse rotates stack B downwards by one.
+ *
+ * The last element of stack B becomes the first one. This function
+ * performs the operation and writes "rrb\n" to standard output.
+ *
+ * @param b Pointer to the pointer of the head of stack B.
+ */
 void	rrb(t_stack_node **b)
 {
 	reverse_rotate(b);
 	write (1, "rrb\n", 4);
 }
 
+/**
+ * @brief Reverse rotates both stacks A and B downwards by one 
+ * simultaneously.
+ *
+ * The last element of each stack becomes the first one.
+ * This function performs the operation on both stacks and writes 
+ * "rrr\n" to standard output.
+ *
+ * @param a Pointer to the pointer of the head of stack A.
+ * @param b Pointer to the pointer of the head of stack B.
+ */
 void	rrr(t_stack_node **a, t_stack_node **b)
 {
 	reverse_rotate(a);
